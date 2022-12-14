160fed33c39ad0b76653b4e62a18a62018ca2f00
Joan D <joandidachr@gmail.comm>
2022-12-14 13:40:56 +0100

    no displaying images + before css

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/', (req, res, next) => {
     res.status(200).sendFile(__dirname + '/views/index.html')
 })
